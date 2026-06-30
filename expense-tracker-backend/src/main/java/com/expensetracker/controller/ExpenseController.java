1c1dd0a21abf6ef7d5558d403286b24eb7ad2a09
Ugeshraja <ugeshraja007@gmail.com>
2026-06-30 21:24:13 +0530

    Add CORS support

--- expense-tracker-backend/src/main/java/com/expensetracker/controller/ExpenseController.java
+++ expense-tracker-backend/src/main/java/com/expensetracker/controller/ExpenseController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 
 @CrossOrigin(origins = {
     "http://localhost:3000",
-    "https://expense-tracker-git-main-ugeshraja1.vercel.app"
+    "https://expense-tracker-kappa-indol.vercel.app/"
 })
 public class ExpenseController {
 
